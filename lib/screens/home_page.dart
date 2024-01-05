62319ecd68dd70cf7ba60b25c7ae08248c1eb0ae
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2024-01-05 18:21:16 +0500

    Add AnimatedOpacity

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,12 +16,51 @@ class _HomePageState extends State<HomePage> {
   }
 
   void initialization() async {
+    await Future.delayed(const Duration(seconds: 2));
     FlutterNativeSplash.remove();
   }
+
+  bool _isVisible = true;
+  Color? _color = Colors.red;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Restaurant Menu")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _isVisible = !_isVisible;
+                  // if(_color == Colors.red){
+                  //   _color = Colors.green;
+                  // }else if(_color==Colors.green)(
+                  // _color = Colors.red
+                  // );
+                });
+              },
+              child: Text(
+                "Animation",
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            SizedBox(
+              height: 26.0,
+            ),
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(milliseconds: 500),
+              child: Container(
+                height: 120,
+                width: 120,
+                color: Colors.red,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
