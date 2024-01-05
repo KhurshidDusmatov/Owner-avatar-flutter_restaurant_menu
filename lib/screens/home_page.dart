f9a76843d59a35acd8d09af0ba8fd35c205044d9
Khurshid Dusmatov <dusmatovkhurshid@gmail.com>
2024-01-05 16:38:13 +0500

    Add Splash Screen

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    FlutterNativeSplash.remove();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Restaurant Menu")),
+    );
+  }
+}
